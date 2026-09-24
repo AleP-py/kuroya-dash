80027bdc8fda14e33085263e061ad3c1b2805912
AleP-py <ianalejandrolarasolis@gmail.com>
2026-09-24 11:15:24 -0600

    Enable debug mode in authOptions

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./prisma";
 
 export const authOptions: NextAuthOptions = {
+  debug: true,                          // ← añade esta línea
   adapter: PrismaAdapter(prisma) as any,
   providers: [
     DiscordProvider({
