9b4800cb458137a3bba50cda0dc3e050376bc152
AleP-py <ianalejandrolarasolis@gmail.com>
2026-09-24 10:54:00 -0600

    Add authentication options with Discord provider

---
+++ lib/auth.ts
@@ -0,0 +1,27 @@
+import { NextAuthOptions } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./prisma";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as any,
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+      authorization: {
+        params: { scope: "identify guilds" },
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      // @ts-ignore
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/",
+  },
+};
